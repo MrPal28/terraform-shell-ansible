20fe255ecbaaf4c949a3a45ab56ee09242e62408
MrPal28 <arindampal669@gmail.com>
2026-09-07 19:06:35 +0530

    Update output values to use list comprehension for EC2 public IPs

--- ec2-terraform/output.tf
+++ ec2-terraform/output.tf
@@ -1,7 +1,10 @@
-output "ec2_public_ip"{
-  value = aws_instance.ec2_automated_server[*].public_ip
-}
-output "ec2_public_dns"{
-  value = aws_instance.ec2_automated_server[*].public_dns
-}
+# output "ec2_public_ip"{
+#   value = aws_instance.ec2_automated_server[*].public_ip
+# }
+# output "ec2_public_dns"{
+#   value = aws_instance.ec2_automated_server[*].public_dns
+# }
 
+output "ec2_public_ip"{
+  value = [for instance in aws_instance.ec2_automated_server : instance.public_ip]
+}
