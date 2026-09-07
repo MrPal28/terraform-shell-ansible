657926374be341bcf1a9f8a973e74f1394a61dc0
MrPal28 <arindampal669@gmail.com>
2026-09-07 15:59:45 +0530

    fix

--- ec2-terraform/output.tf
+++ ec2-terraform/output.tf
@@ -1,7 +1,7 @@
 output "ec2_public_ip"{
-  value = aws_instance.ec2_automated_server[var.ec2_instance_count - 1].public_ip
+  value = aws_instance.ec2_automated_server[*].public_ip
 }
 output "ec2_public_dns"{
-  value = aws_instance.ec2_automated_server[var.ec2_instance_count - 1].public_dns
+  value = aws_instance.ec2_automated_server[*].public_dns
 }
 
