022c3af98efdad870c84781fb3a0148e5dad1399
MrPal28 <arindampal669@gmail.com>
2026-09-08 15:47:10 +0530

    Fix key name reference for EC2 instance to use correct key pair resource

--- ec2-terraform/ec2.tf
+++ ec2-terraform/ec2.tf
@@ -55,7 +55,7 @@ resource "aws_instance" "ec2_automated_server" {
         AWS_EC2_2 = "t3.micro"
     })
     #count = var.ec2_instance_count #Meta Argument to create multiple instances
-    key_name = key_pair.terraform_key.key_name
+    key_name = key_pair.aws_key_pair.terraform_key.key_name
     security_groups = [aws_security_group.allow_ssh.name]
     instance_type = each.value
     ami = var.ec2_ami
