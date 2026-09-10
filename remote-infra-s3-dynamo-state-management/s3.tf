ac70af732b3602316cab98b834958d8d9781359f
MrPal28 <arindampal669@gmail.com>
2026-09-10 18:30:51 +0530

    fixing resource name s3 remote infra

--- remote-infra-s3-dynamo-state-management/s3.tf
+++ remote-infra-s3-dynamo-state-management/s3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "terraform_state"{
-    bucket = "terraform-state-bucket"
+resource "aws_s3_bucket" "terraform_state_bucket_ap"{
+    bucket = "terraform-state-bucket-ap"
 
     tags = {
-        Name = "terraform-state-bucket"
+        Name = "terraform-state-bucket-ap"
         Environment = "Dev"
     }
 }
