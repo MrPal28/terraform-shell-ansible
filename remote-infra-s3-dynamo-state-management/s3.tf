cf01e2c9b42fb64979a4117fa79153941bd58f18
MrPal28 <arindampal669@gmail.com>
2026-09-10 18:39:12 +0530

    Update S3 bucket name to include unique identifier for clarity

--- remote-infra-s3-dynamo-state-management/s3.tf
+++ remote-infra-s3-dynamo-state-management/s3.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "terraform_state_bucket_ap_28"{
-    bucket = "terraform-state-bucket-ap"
-
+bucket = "terraform-state-bucket-ap-arindam-2026"
     tags = {
         Name = "terraform-state-bucket-ap"
         Environment = "Dev"
