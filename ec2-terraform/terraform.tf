d965eff10b1255aaaa56a94af51215013e4269cf
MrPal28 <arindampal669@gmail.com>
2026-09-10 18:52:44 +0530

    Fix incorrect attribute name for DynamoDB table in S3 backend configuration

--- ec2-terraform/terraform.tf
+++ ec2-terraform/terraform.tf
@@ -9,6 +9,6 @@ terraform {
   bucket         = "terraform-state-bucket-ap-arindam-2026"
   key            = "terraform.tfstate"
   region         = "ap-south-1"
-  use_lockfile = "terraform-dynamodb-state-table"
+  dynamodb_table = "terraform-dynamodb-state-table"
 }
 }
